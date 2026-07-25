b807406e1f5ea9ca33dcb01bc6db5b9dc161e6b8
shaiju <shaijuvr@rediffmail.com>
2026-07-25 10:08:56 +0530

    added vanilla hello-gpt

---
+++ src/vanilla/hello-gpt.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv(override=True)  # Load environment variables from .env file
+
+system_msg = "You are a helpful assistant. You tell fun facts about the world. You are very concise and only respond with the fun fact. You do not provide any other information."
+
+get_response = requests.post("https://api.openai.com/v1/chat/completions", json={
+    "model": "gpt-4o-mini",
+    "messages": [
+        {"role": "system", "content": system_msg},
+        {"role": "user", "content": "Tell me a fun fact about the India."}
+    ]
+}, headers={"Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}", "Content-Type": "application/json"})
+if get_response.status_code == 200:
+    print(get_response.json()["choices"][0]["message"]["content"])
